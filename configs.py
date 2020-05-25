e2d1eeef828e73577b3a25dc1e7a2a3f81eb0262
juansgaitan <juans.gaitan@gmail.com>
2020-05-25 16:38:51 +0200

    Add slowloris http test tool

---
+++ configs.py
@@ -0,0 +1,7 @@
+# Global configurations
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36"
+
+ACCEPT_LANGUAGE = (
+    "en-US,en;q=0.9,es;q=0.8,fr-FR;q=0.7,fr;q=0.6,la;q=0.5,de;q=0.4,fy;q=0.3,sv;q=0.2"
+)
+
