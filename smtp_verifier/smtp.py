4c0a9fa8992400a7f1fb3becbb3da97f745345f1
Juan Villamizar <juan.villamizar@revolut.com>
2021-12-24 16:54:05 +0100

    Add verifications.log middleware

--- smtp_verifier/smtp.py
+++ smtp_verifier/smtp.py
@@ -1,13 +1,29 @@
+from pathlib import Path
+from datetime import datetime
 from time import sleep
 from typing import Callable, Tuple
 from itertools import cycle
 from smtplib import SMTP
 
 
-EMAIL_VERIFICATIONS = "./cache/verifications.ldjson"
+LOG_ENTRY_PATH = Path("./cache/verifications.log")
 REQUEST_TIMEOUT_SMTP = 25
 EMAILS_LIST = "emails.csv"
 DATE_FORMAT = "%Y-%m-%d"
+
+
+def _log_entry(fn):
+    LOG_ENTRY_PATH.touch(exist_ok=True)
+
+    def wrapper(*args, **kwargs):
+        result = fn(*args, **kwargs)
+        with LOG_ENTRY_PATH.open(mode="a") as log:
+            timestamp = datetime.now().isoformat()
+            info = ", ".join([f"{k}={v}" for k, v in result.items()])
+            log.writelines([f"[{timestamp}] {info}", "\n"])
+        return result
+
+    return wrapper
 
 
 def smtp_email_check(mx: str, username: str, domain: str) -> Tuple[int, bytes]:
@@ -28,6 +44,7 @@ hosts_by_domain = {}
 MAX_RETRIES = 10
 
 
+@_log_entry
 def verify_email(email: str, get_mx_servers: Callable) -> dict:
     if "@" not in email:
         return {"email": email, "error": "Not an email."}
